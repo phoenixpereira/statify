e8d52e49e67febf99afc7d2f186a17f49949cf2f
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-09-16 15:43:30 +0930

    Add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+import { Button } from 'antd';
+
+const App = () => (
+	<div className="App">
+		<h1>Hello World!</h1>
+		<Button type="primary">Button</Button>
+	</div>
+);
 
 export default App;
