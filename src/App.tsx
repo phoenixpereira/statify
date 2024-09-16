fb9bc581b4179eeb85134d82977f6537abfbdbb2
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-09-16 16:49:20 +0930

    Add token to local storage and logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 import { Button } from 'antd';
+import { useEffect, useState } from 'react';
 
 import { loginUrl } from './spotify';
 
-const App = () => (
-	<div className="App">
-		<h1>Hello World!</h1>
-		<Button type="primary" href={loginUrl}>
-			Log in to Spotify
-		</Button>
-	</div>
-);
+function App() {
+	const [token, setToken] = useState('');
+
+	useEffect(() => {
+		const hash = window.location.hash;
+		let token = window.localStorage.getItem('token');
+
+		if (!token && hash) {
+			token = hash
+				.substring(1)
+				.split('&')
+				.find((elem) => elem.startsWith('access_token'))
+				.split('=')[1];
+
+			window.location.hash = '';
+			window.localStorage.setItem('token', token);
+		}
+
+		setToken(token);
+	}, []);
+
+	const logout = () => {
+		setToken('');
+		window.localStorage.removeItem('token');
+	};
+
+	return (
+		<div className="App">
+			<h1>Hello World!</h1>
+			{!token ? (
+				<Button type="primary" href={loginUrl}>
+					Log in to Spotify
+				</Button>
+			) : (
+				<Button type="primary" onClick={logout}>
+					Logout
+				</Button>
+			)}
+		</div>
+	);
+}
 
 export default App;
