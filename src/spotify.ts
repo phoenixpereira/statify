a8e20f28d31c6caa450ec8d30043defad9bf1258
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-09-17 17:04:46 +0930

    Remove unneeded scopes

--- src/spotify.ts
+++ src/spotify.ts
@@ -4,12 +4,6 @@ const redirectUri = import.meta.env.VITE_REDIRECT_URI;
 
 const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 
-const scopes = [
-	'user-read-currently-playing',
-	'user-read-recently-played',
-	'user-read-playback-state',
-	'user-top-read',
-	'user-modify-playback-state',
-];
+const scopes = ['user-read-recently-played', 'user-top-read'];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
