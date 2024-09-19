6cc3b10a77502cd0f99b9ec510399ebbe861b0de
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-09-19 14:01:34 +0930

    Fix scope

--- src/spotify.ts
+++ src/spotify.ts
@@ -4,6 +4,10 @@ const redirectUri = import.meta.env.VITE_REDIRECT_URI;
 
 const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 
-const scopes = ['user-read-recently-played', 'user-top-read'];
+const scopes = [
+	'user-read-recently-played',
+	'user-top-read',
+	'user-follow-read',
+];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
